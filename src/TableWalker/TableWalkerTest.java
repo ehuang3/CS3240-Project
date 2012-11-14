0788b75da6ec27deb52c722c081178c8c1292100
Hyangjin Lee <Hyangjin Lee@128.61.23.101>
2012-11-13 23:40:28 -0500

    update!

--- src/TableWalker/TableWalkerTest.java
+++ src/TableWalker/TableWalkerTest.java
@@ -1,5 +1,17 @@
 package TableWalker;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Generator.DFA.Node;
+import Generator.DFA.DFA;
+import Generator.DFA.Transition;
+
 public class TableWalkerTest {
 
+	
+	tablewalker walker = new tablewalker()
 }
