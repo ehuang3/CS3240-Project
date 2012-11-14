6c96b0829e5516e0078b77da21986653852ff768
Hoyin29 <hoyin29@gmail.com>
2012-11-13 19:48:34 -0500

    another major char factory revision!!

--- src/Generator/Character/CharacterClassFactory.java
+++ src/Generator/Character/CharacterClassFactory.java
@@ -22,59 +22,93 @@ public class CharacterClassFactory
 	
 	public CharacterClass build(String regex)  
 	{
-		token = tokenizer.next();  //consume 1st token (may be a name or [) 
+		tokenizer.tokenize(regex);
+		
+		System.out.println("\tinside build()");
+		peekToken = tokenizer.peek();  //consume 1st token (may be a name or [) 
 		CharacterClass temp = new CharacterClass();
 		
-		if(token.operand == op_code.id)  //name of char class (1st token)
+		if(peekToken.operand == op_code.id)  //name of char class (1st token)
 		{
+			consume(1);  //consume name of class 1
 			temp.name = token.value;  //set char class name
 			map.put(temp.name, temp);  //add char class with a name to map
-
-			token = tokenizer.next();  //consume [
-			if(token.operand == op_code.left_brac) 
-			{
-				temp = basic();  //get first [^...] or [...] char class object  
-			}
-		}
-		else  //[ is 1st token, char class without a name (not added to map)
-		{
-			//temp = charClass();  
-			temp = basic();  //get first [^...] or [...] char class object 
+			System.out.println("\t\t\t\t\t\t\t\t\tname: " + token.value + "\tsize: " + map.size());
 		}
 		
-		//temp is a char class of [^...] or [...], need to check for IN 
-		//current token could be IN if not finished
-		if(token.operand == op_code.in)  //IN
-		{
-			CharacterClass temp2;
-			token = tokenizer.next();  //consume IN
-			temp2 = charList();  //original class to exclude from
-			temp2.exclude(temp);  //exclude everything of temp inside temp2
-			temp = temp2; 
-		}
+		temp = charClass();  //get first [^...] or [...] char class object 
 		return temp;
 	}
 	
-	/*
-	public CharacterClass charClass()  
-	{
-		return basic();  //return [^...] or [...]
+	public CharacterClass charClass()  //start at [ not consumed (may be part of named or no named line
+	{	
+		CharacterClass temp = null;
+		
+		peekToken = tokenizer.peek();  //peek at next token to determine if it is [
+		if(peekToken.operand == op_code.left_brac) 
+		{
+			//token = tokenizer.next();  //consume [
+			consume(1);
+			temp = basic();  //get first [^...] or [...] char class object  
+		}
+		
+		/*
+		System.out.println("back from first char class");
+		System.out.println("class 1: ");
+		temp.print();
+		System.out.println("\n\n");
+		*/
+		
+		//just consumed ]
+		peekToken = tokenizer.peek();
+		
+		//temp is a char class of [^...] or [...], need to check for IN 
+		//current token could be IN if not finished
+		if(peekToken.operand == op_code.in)  //IN
+		{
+			CharacterClass temp2 = null;
+			
+			//token = tokenizer.next();  //consume IN
+			consume(1);
+			
+			peekToken = tokenizer.peek();  //check if next token is $___ or [
+			if(peekToken.operand == op_code.left_brac)
+			{
+				//token = tokenizer.next();  //consume [ 
+				consume(1);
+				
+				temp2 = charList();  //generate another [...] to exclude from
+			}
+			else if(peekToken.operand == op_code.id) 
+			{
+				consume(1);  //consume name of class 2
+				if(map.containsKey(token.value))
+				{
+					temp2 = map.get(token.value);  //get existing [...] from map
+				}
+			}
+
+			temp = temp2.exclude(temp);  //exclude everything of temp inside temp2
+		}	
+		return temp;
 	}
-	*/
 	
 	public CharacterClass basic()  //build [...], already consumed [
 	{
+		System.out.println("\tinside basic()");
 		peekToken = tokenizer.peek();  //check if next token is ^
 		
 		if(peekToken.operand == op_code.exclude)  //[^ exclude
 		{
-			token = tokenizer.next();  //consume ^
+			//token = tokenizer.next();  //consume ^
+			consume(1);
 		}
 		return charList();
 	}
 	
 	public CharacterClass charList()  //start by consuming 1st cls_char token
 	{
+		System.out.println("\tinside charList()");
 		CharacterClass temp = new CharacterClass();
 		peekToken = tokenizer.peek();  //check if next token is cls_char
 		
@@ -84,21 +118,40 @@ public class CharacterClassFactory
 			if(peekToken.operand == op_code.range)
 			{ 
 				start = token.value.charAt(0);  //set current token as start bound
-				token = tokenizer.next();  //consume range
-				token = tokenizer.next();  //consume end
+				System.out.println("\t\t\t\t\t\t\t\t\tstart: " + start);
+				
+				//token = tokenizer.next();  //consume range
+				//token = tokenizer.next();  //consume end
+				consume(2);
+				
 				end = token.value.charAt(0);  //set current token as end bound
+				System.out.println("\t\t\t\t\t\t\t\t\tend: " + end);
 				
 				temp.acceptBoundary(start, end);  //add range to char class
 			}	
 			
-			if(token.operand == op_code.cls_char)
+			if(peekToken.operand == op_code.cls_char)
 			{
-				token = tokenizer.next();  //consume current cls_char
+				//token = tokenizer.next();  //consume current cls_char
+				consume(1);
+				
 				temp.accept(token.value.charAt(0));  //add current cls_char to char class
 			}
+			
+			peekToken = tokenizer.peek();  //check if next token is cls_char
 		}
 		
-		token = tokenizer.next();  //consume ]
+		//token = tokenizer.next();  //consume ]
+		consume(1);
 		return temp;  //return finished char class
+	}
+	
+	public void consume(int i)
+	{
+		for(int a = 0; a < i; a++)
+		{
+			token = tokenizer.next();
+			System.out.println("\t\t\tconsumed: " + token.value + "\ttype: " + token.operand);
+		}
 	}
 }
