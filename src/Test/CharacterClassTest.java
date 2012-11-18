fdf24b0086c24c26f726f0efbc42034b22ead35f
Hoyin29 <hoyin29@gmail.com>
2012-11-17 19:58:39 -0500

    junit tests for char class factory

--- src/Test/CharacterClassTest.java
+++ src/Test/CharacterClassTest.java
@@ -15,54 +15,68 @@ import Generator.NFA.NFASimulator;
 
 public class CharacterClassTest 
 {
+	Tokenizer t;
+	CharacterClassFactory ccf;
+	CharacterClass cc;	
+	
 	@Before
 	public void setUp() throws Exception 
 	{
-		
-	}
 
+	}
+	
 	@Test
-	public void testCharacterClass()
+	public void testName()
 	{
 		String regex1 = "$NAME [abc]";
+		assertEquals("wrong result", "abc", testProcedure(regex1));
 		String regex2 = "[abc]";
-				
-		String regex3 = "$NAME [kswl-q]";
-		String regex4 = "[oolav-zldg]";
+		assertEquals("wrong result", "abc", testProcedure(regex2));
+	}
 	
+	@Test
+	public void testRange()
+	{
+		String regex3 = "$NAME [kswl-q]";
+		assertEquals("wrong result", "klmnopqsw", testProcedure(regex3));
+		String regex4 = "[oolav-zldg]";
+		assertEquals("wrong result", "adglovwxyz", testProcedure(regex4));
+	}
+	
+	@Test
+	public void testNameExclude()
+	{
 		String regex5 = "$EXCLUDE [^abc] IN $NAME";
+		assertEquals("wrong result", "defghijklmnopqrstuvwxyz", testProcedure(regex5));
 		String regex6 = "$EXCLUDE [^abc] IN [a-z]";
-		
+		assertEquals("wrong result", "defghijklmnopqrstuvwxyz", testProcedure(regex6));
+	}
+	
+	@Test
+	public void testNoNameExclude()
+	{
 		String regex7 = "[^skn] IN $NAME"; 
+		assertEquals("wrong result", "abcdefghijlmopqrtuvwxyz", testProcedure(regex7));
 		String regex8 = "[^owq] IN [a-z]";
-		
+		assertEquals("wrong result", "abcdefghijklmnprstuvxyz", testProcedure(regex8));
+	}
+	
+	@Test
+	public void testComplexNoName()
+	{
 		String regex9 = "[a-dc-wA-Jj-u]";
-		String regex10 = "[a-dXXc-wYYA-Jj-u]";
-		
-		ArrayList<String> all = new ArrayList<String>();
-		all.add(regex1);
-		all.add(regex2);
-		all.add(regex3);
-		all.add(regex4);
-		all.add(regex5);
-		all.add(regex6);
-		all.add(regex7);
-		all.add(regex8);
-		all.add(regex9);
-		all.add(regex10);
-		
-		Tokenizer t;
-		CharacterClassFactory ccf;
-		CharacterClass cc;
-		
-		for(String s: all)
-		{
-			t = new Tokenizer(s);
-			ccf = new CharacterClassFactory(t);
-			cc = new CharacterClass("$NAME");
-			cc.acceptBoundary('a', 'z');
-			ccf.map.put("$NAME", cc);
-			ccf.build(s).print();
-		}
+		assertEquals("wrong result", "ABCDEFGHIJabcdefghijklmnopqrstuvw", testProcedure(regex9));
+		String regex10 = "[a-dXXc-wYYA-Jj-u]";		
+		assertEquals("wrong result", "ABCDEFGHIJXYabcdefghijklmnopqrstuvw", testProcedure(regex10));
+	}
+	
+	public String testProcedure(String s)
+	{
+		t = new Tokenizer(s);
+		ccf = new CharacterClassFactory(t);
+		cc = new CharacterClass("$NAME");
+		cc.acceptBoundary('a', 'z');
+		ccf.map.put("$NAME", cc);
+		return ccf.build(s).print();
 	}
 }
