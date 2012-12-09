d42c7167fcdc21ef2f03148f76f9ad1609cd8a2e
Hyangjin Lee <Hyangjin Lee@192.168.2.2>
2012-12-09 18:57:25 -0500

    MiniVM

--- src/MiniRE/VirtualMachine/MiniVM.java
+++ src/MiniRE/VirtualMachine/MiniVM.java
@@ -56,39 +56,40 @@ public class MiniVM {
 		
 		switch (ast.get(0).rule_id) {
 		case "REPLACE" :
-			// TODO
+			match("REPLACE", ast.get(0));
 			break;
 		case "RECREP" :
-			// TODO
+			match("RECREP", ast.get(0));
 			break;
 		case "ID" :
+			statement_righthand(ast.get(0));
+			break;
+		case "PRINT" :
 			// TODO
 			break;
+		
 		}
 	}
 	
-	public Variable statement_righthand(AST ast) {
+	public Variable statement_righthand(AST ast) throws Exception {
 		match("statement-righthand", ast);
 		
 		switch (ast.get(0).rule_id) {
 		case "exp" :
-			// TODO
+			exp(ast.get(0));
 			break;
 		case "HASH" :
-			// TODO
+			match("HASH", ast.get(0));
 			break;
 		case "MAXFREQ" :
-			// TODO
-			break;
-		case "PRINT" :
-			// TODO
+			match("MAXFREQ", ast.get(0));
 			break;
 		}
 		
 		return null;
 	}
 	
-	public String[] file_names(AST ast) {
+	public String[] file_names(AST ast) throws Exception{
 		match("file-names", ast);
 		
 		//TODO
@@ -96,19 +97,94 @@ public class MiniVM {
 		return null;
 	}
 	
-	public String source_file(AST ast) {
+	public String source_file(AST ast) throws Exception{
 		match("source-file", ast);
+		
+		match("ASCII-STR", ast.get(0));		
+		return null;
+	}
+	
+	public void destination_file(AST ast) throws Exception{
+		match("destination-file", ast);
+		
+		match("ASCII-STR", ast.get(0));
+
+	}
+	
+	public void exp_list(AST ast) throws Exception{
+		match("exp-list", ast);
+		
+		exp(ast.get(0));
+		exp_list_tail(ast.get(0));
+
+	}
+	
+	public void exp_list_tail(AST ast) throws Exception{
+		match("exp-list-tail", ast);
+		
+		// comma?????
+	}
+	
+	public void exp(AST ast) throws Exception{
+		match("exp", ast);
+		
+		switch(ast.get(0).rule_id) {
+		case "ID" :
+			match("ID", ast.get(0));
+			break;
+		case "exp" :
+			exp(ast.get(0));
+			break;
+		case "term" :
+			term(ast.get(0));
+			exp_tail(ast.get(1));
+			break;
+		}
+	}
+	
+	public void exp_tail(AST ast) throws Exception{
+		match("exp-tail", ast);
+		
+		switch(ast.get(0).rule_id) {
+		case "bin-op" :
+			bin_op(ast.get(0));
+			term(ast.get(1));
+			exp_tail(ast.get(2));
+			break;
+		case "epsilon" :
+			match("epsilon", ast.get(0));
+			break;
+		}
+
+	}
+	
+	public Variable term(AST ast) throws Exception{
+		match("term", ast);
+		
+		
+	}
+	
+	public String file_name(AST ast) throws Exception{
+		match("file-name", ast);
 		
 		//TODO
 		
 		return null;
 	}
 	
-	public String destination_file(AST ast) {
-		match("destination-file", ast);
+	public void bin_op(AST ast) throws Exception{
+		match("bin-op", ast);
 		
-		//TODO
-		
-		return null;
+		switch(ast.get(0).rule_id){
+		case "DIFF" :
+			match("DIFF", ast.get(0));
+			break;
+		case "UNION" :
+			match("UNION", ast.get(0));
+			break;
+		case "INTERS" :
+			match("INTERS", ast.get(0));
+			break;
+		}
 	}
 }
