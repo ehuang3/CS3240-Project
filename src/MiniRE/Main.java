6325b04eaea9224c9ae254775e6861b9cd23accf
eric <ehuang3@gatech.edu>
2012-12-09 23:02:38 -0500

    Fixed main

--- src/MiniRE/Main.java
+++ src/MiniRE/Main.java
@@ -7,7 +7,10 @@ public class Main
 {
 	public static void main(String[] args) throws Exception 
 	{
-		String re = args[1];
+		String re = args[0];
+		
+		System.out.println(re);
+		
 		MiniVM mvm = new MiniVM();
 		MiniParser mp = new MiniParser();
 		AST tree = mp.parseFile(re);
